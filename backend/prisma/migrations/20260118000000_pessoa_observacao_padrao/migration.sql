2fd58b35bb1dabb0a54786b5dcdcbd0ed0af241f
grupobionorte <grupobionorte@gmail.com>
2026-09-25 18:31:47 -0400

    Notas Fiscais

---
+++ backend/prisma/migrations/20260118000000_pessoa_observacao_padrao/migration.sql
@@ -0,0 +1,2 @@
+-- Observação padrão do cliente, usada nas informações complementares da NF-e.
+ALTER TABLE "pessoas" ADD COLUMN "observacao_padrao" TEXT;
---
+++ backend/prisma/migrations/20260119000000_empresa_observacao_nfe/migration.sql
@@ -0,0 +1,2 @@
+-- Mensagem fixa da empresa nas informações complementares da NF-e.
+ALTER TABLE "empresas" ADD COLUMN "observacao_padrao_nfe" TEXT;
